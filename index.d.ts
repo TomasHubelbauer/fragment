b1bc022942fd6e2cd292b91b80f594c3675a9de1
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-08 10:58:47 +0100

    Declare the main function and the tag helper functions

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+declare function reconcile(target: HTMLElement, ...fragments: Node[]);
+
+declare function div(...attributesAndOrChildren: (object | Node)[]);
+declare function h1(...attributesAndOrChildren: (object | Node)[]);
+declare function h2(...attributesAndOrChildren: (object | Node)[]);
+declare function h3(...attributesAndOrChildren: (object | Node)[]);
+declare function h4(...attributesAndOrChildren: (object | Node)[]);
+declare function h5(...attributesAndOrChildren: (object | Node)[]);
+declare function h6(...attributesAndOrChildren: (object | Node)[]);
+declare function p(...attributesAndOrChildren: (object | Node)[]);
+declare function button(...attributesAndOrChildren: (object | Node)[]);
+declare function code(...attributesAndOrChildren: (object | Node)[]);
+declare function b(...attributesAndOrChildren: (object | Node)[]);
+declare function a(...attributesAndOrChildren: (object | Node)[]);
+declare function details(...attributesAndOrChildren: (object | Node)[]);
+declare function summary(...attributesAndOrChildren: (object | Node)[]);
+declare function span(...attributesAndOrChildren: (object | Node)[]);
