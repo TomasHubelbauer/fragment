ef027ae0675076eeaf66556b3a178a954445a266
Tomáš Hübelbauer <tomas@hubelbauer.net>
2019-01-11 23:28:21 +0100

    Add the missing outerHTML reads

--- index.test.js
+++ index.test.js
@@ -1,25 +1,25 @@
 import { create } from './lib.js';
 
 test('recognizes first object argument as attributes and second string argument as children', _ =>
-  expect(create('element1', [ { props: true }, 'child' ])).toBe('<element1 props="true">child</element1>')
+  expect(create('element1', [ { props: true }, 'child' ]).outerHTML).toBe('<element1 props="true">child</element1>')
 );
 
 test('recognizes first object argument as attributes and second array of string argument as children', _ =>
-  expect(create('element2', [ { props: true }, [ 'child' ] ])).toBe('<element2 props="true">child</element2>')
+  expect(create('element2', [ { props: true }, [ 'child' ] ]).outerHTML).toBe('<element2 props="true">child</element2>')
 );
 
 test('recognizes first object argument as attributes and second and third string arguments as children', _ =>
-  expect(create('element3', [ { props: true }, 'child1', 'child2' ])).toBe('<element3 props="true">child1child2</element3>')
+  expect(create('element3', [ { props: true }, 'child1', 'child2' ]).outerHTML).toBe('<element3 props="true">child1child2</element3>')
 );
 
 test('recognizes first array of string argument as children', _ =>
-  expect(create('element4', [ 'child' ])).toBe('<element4>child</element4>')
+  expect(create('element4', [ 'child' ]).outerHTML).toBe('<element4>child</element4>')
 );
 
 test('recognizes first array of array of string argument as children', _ =>
-  expect(create('element5', [ [ 'child' ] ])).toBe('<element5>child</element5>')
+  expect(create('element5', [ [ 'child' ] ]).outerHTML).toBe('<element5>child</element5>')
 );
 
 test('recognizes first array of strings argument as children', _ =>
-  expect(create('element6', [ 'child1', 'child2' ])).toBe('<element6>child1child2</element6>')
+  expect(create('element6', [ 'child1', 'child2' ]).outerHTML).toBe('<element6>child1child2</element6>')
 );
